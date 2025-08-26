edfc7b02706ae2ace55900d517fc0a8d76335076
Kangyoon Lee <rkddbs1031@gmail.com>
2025-08-26 15:27:06 +0900

    chore: 불필요한 파일 삭제 및 postcss 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <main>{children}</main>
       </body>
     </html>
   );
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import { error } from 'console';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,75 +10,73 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'prettier', 'next/typescript'),
+  ...compat.extends("next/core-web-vitals", "prettier", "next/typescript"),
   {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-    ],
-    plugins: ['@typescript-eslint', 'react'],
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+    plugins: ["@typescript-eslint", "react"],
     rules: {
       // TS & React 규칙
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': 'error',
-      'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-props-no-spreading': 'off',
-      'react/no-array-index-key': 'warn',
-      'react/jsx-props-no-spreading': 'off',
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "error",
+      "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/no-array-index-key": "warn",
+      "react/jsx-props-no-spreading": "off",
+      "react/self-closing-comp": [
+        "error",
+        {
+          component: true,
+          html: true,
+        },
+      ],
 
       // import
-      'import/order': [
-        'error',
+      "import/order": [
+        "error",
         {
           groups: [
-            ['builtin', 'external'], // Node.js 내장 모듈 + 외부 패키지
-            ['internal', 'type'], // 내부 모듈(@/components, @/utils 등) + 타입
-            ['parent', 'sibling', 'index'], // 상위, 같은 폴더, 인덱스
+            ["builtin", "external"], // Node.js 내장 모듈 + 외부 패키지
+            ["internal", "type"], // 내부 모듈(@/components, @/utils 등) + 타입
+            ["parent", "sibling", "index"], // 상위, 같은 폴더, 인덱스
           ],
           pathGroups: [
             {
-              pattern: '@/components/**',
-              group: 'internal',
-              position: 'after',
+              pattern: "@/components/**",
+              group: "internal",
+              position: "after",
             },
             {
-              pattern: '@/hooks/**',
-              group: 'internal',
-              position: 'after',
+              pattern: "@/hooks/**",
+              group: "internal",
+              position: "after",
             },
             {
-              pattern: '@/utils/**',
-              group: 'internal',
-              position: 'after',
+              pattern: "@/utils/**",
+              group: "internal",
+              position: "after",
             },
             {
-              pattern: '@/types/**',
-              group: 'type',
-              position: 'after',
+              pattern: "@/types/**",
+              group: "type",
+              position: "after",
             },
             {
-              pattern: '@/states/**',
-              group: 'type',
-              position: 'after',
+              pattern: "@/states/**",
+              group: "type",
+              position: "after",
             },
           ],
-          'newlines-between': 'always', // 그룹 사이 한 줄 띄우기
+          "newlines-between": "always", // 그룹 사이 한 줄 띄우기
           alphabetize: {
-            order: 'asc', // 그룹 내부 알파벳 순
+            order: "asc", // 그룹 내부 알파벳 순
             caseInsensitive: true,
           },
         },
       ],
 
       // Prettier 연동
-      'prettier/prettier': [
-        'error',
-        { singleQuote: true, semi: true, trailingComma: 'all' },
-      ],
+      "prettier/prettier": ["error", { singleQuote: true, semi: true, trailingComma: "all" }],
     },
   },
 ];

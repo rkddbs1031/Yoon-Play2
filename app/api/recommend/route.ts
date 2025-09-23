ce552475ef79ce4396b404c22d9ea216bcd2bbf6
Kangyoon Lee <rkddbs1031@gmail.com>
2025-09-23 20:30:18 +0900

    Feat: openai api content 수정

--- app/api/recommend/route.ts
+++ app/api/recommend/route.ts
@@ -16,10 +16,11 @@ export async function POST(request: NextRequest) {
         {
           role: 'user',
           content: `
-            ${query}에 맞는 음악을 추천해줘.
-            - 설명: 추천 배경이나 분위기에 대한 설명
-            - 리스트: 음악 장르 3개, 플레이리스트 이름 3개만(유튜브 플레이리스트 제목), 배열 형태로 제공, 장르와 플레이리스트 외의 다른 데이터는 없어야할 것
-            - 유튜브 링크는 절대 포함하지 않을 것
+            ${query}의 내용은 사용자의 상태, 감정, 좋아하는 가수, 현재 날씨가 될 수 있어. 그에 맞는 음악을 추천해줘.
+            - 설명: 추천 배경이나 분위기에 대한 설명.
+            - 리스트: 음악 장르 3개, 플레이리스트 이름 3개만(유튜브 플레이리스트 제목), 배열 형태로 제공, 장르와 플레이리스트 외의 다른 데이터는 없어야할 것.
+            - 리스트 하위의 장르와 플레이리스트는 중복되지 않아야 함.
+            - 유튜브 링크는 절대 포함하지 않을 것.
             - JSON 형식으로 꼭 지키며 답변해:
               {
                 "description": "...",
@@ -37,7 +38,7 @@ export async function POST(request: NextRequest) {
     const result = JSON.parse(response.choices[0].message?.content || '{}');
     return NextResponse.json(result);
   } catch (error: any) {
-    console.error('OpenAI API 오류:', error);
+    console.error('OpenAI API 오류:', error, error.status);
 
     // 오류 유형에 따라 다른 메시지와 상태 코드 반환
     if (error.status === 429) {

2c90f4a5724f883a47d00b3257cbe47e5f9089ca
Kangyoon Lee <rkddbs1031@gmail.com>
2025-09-23 19:38:57 +0900

    Feat: Result Keyword Card Button

---
+++ components/Card.tsx
@@ -0,0 +1,21 @@
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+}
+
+const KeywordButton = ({ onClick, label, ...props }: ButtonProps) => {
+  return (
+    <button
+      type='button'
+      className='w-full py-4 sm:py-3 px-2 text-[14px] rounded-[12px] sm:rounded-[16px] backdrop-blur-xl shadow-[0_4px_12px_rgba(0,0,0,0.1)] cursor-pointer transition-bg duration-400 bg-white/20 hover:bg-white/90'
+      {...props}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export const Card = {
+  Keyword: KeywordButton,
+};

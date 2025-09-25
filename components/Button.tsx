5005fbde7362da7d3e45ca210870c14d043976a0
Kangyoon Lee <rkddbs1031@gmail.com>
2025-09-25 18:21:04 +0900

    Feat: 기존 KeywordButton Button으로 변경

---
+++ components/Button.tsx
@@ -0,0 +1,18 @@
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+}
+
+const Button = ({ onClick, label, ...props }: ButtonProps) => {
+  return (
+    <button
+      type='button'
+      className='w-full py-4 sm:py-3 px-2 text-[14px] rounded-[32px] backdrop-blur-xl shadow-[0_4px_12px_rgba(0,0,0,0.1)] cursor-pointer transition-bg duration-400 bg-white/20 hover:bg-white/70'
+      {...props}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+export default Button;

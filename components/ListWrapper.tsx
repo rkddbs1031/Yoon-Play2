dd76ff9b94785e979913a9d68841580e400ff8f7
Kangyoon Lee <rkddbs1031@gmail.com>
2025-09-25 17:23:45 +0900

    Feat: Card onClick시 type도 같이 전달되도록 수정

--- components/ListWrapper.tsx
+++ components/ListWrapper.tsx
@@ -1,23 +1,25 @@
+import { RecommendationResultType } from '@/types/recommend';
+
 import { Card } from './Card';
 
 interface ListWrapperProps {
-  sectionName: string;
+  type: RecommendationResultType;
   title: string;
   list: string[];
-  onClick: (label: string) => void;
+  onClick: ({ value, type }: { value: string; type: RecommendationResultType }) => void;
 }
 
-export const ListWrapper = ({ sectionName, title, list, onClick }: ListWrapperProps) => {
+export const ListWrapper = ({ type, title, list, onClick }: ListWrapperProps) => {
   return (
     <div
-      className={`${sectionName}-wrapper flex flex-col gap-4 bg-[#ffffff33] px-5 pt-5 pb-6 rounded-[24px] shadow-lg shadow-[0_4px_20px_rgba(0, 0, 0, 0.08)] backdrop-blur-xl`}
+      className={`${type}-wrapper flex flex-col gap-4 bg-[#ffffff33] px-5 pt-5 pb-6 rounded-[24px] shadow-lg shadow-[0_4px_20px_rgba(0, 0, 0, 0.08)] backdrop-blur-xl`}
     >
       <h3 className='font-bold text-[12px]'>{title}</h3>
       {list && (
         <ul className='flex flex-col sm:flex-row gap-5 sm:gap-4'>
           {list.map(label => (
             <li key={label} className='flex-1'>
-              <Card.Keyword label={label} onClick={() => onClick(label)} />
+              <Card.Keyword label={label} onClick={() => onClick({ value: label, type })} />
             </li>
           ))}
         </ul>

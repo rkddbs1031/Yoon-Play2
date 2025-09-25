63667d42a1d556f9a41ba88e530a822e9e42de03
Kangyoon Lee <rkddbs1031@gmail.com>
2025-09-25 18:21:26 +0900

    Feat: ListWrapper Button 적용

--- components/ListWrapper.tsx
+++ components/ListWrapper.tsx
@@ -1,5 +1,6 @@
 import { RecommendationResultType } from '@/types/recommend';
 
+import Button from './Button';
 import { Card } from './Card';
 
 interface ListWrapperProps {
@@ -11,19 +12,17 @@ interface ListWrapperProps {
 
 export const ListWrapper = ({ type, title, list, onClick }: ListWrapperProps) => {
   return (
-    <div
-      className={`${type}-wrapper flex flex-col gap-4 bg-[#ffffff33] px-5 pt-5 pb-6 rounded-[24px] shadow-lg shadow-[0_4px_20px_rgba(0, 0, 0, 0.08)] backdrop-blur-xl`}
-    >
+    <Card>
       <h3 className='font-bold text-[12px]'>{title}</h3>
       {list && (
         <ul className='flex flex-col sm:flex-row gap-5 sm:gap-4'>
           {list.map(label => (
             <li key={label} className='flex-1'>
-              <Card.Keyword label={label} onClick={() => onClick({ value: label, type })} />
+              <Button label={label} onClick={() => onClick({ value: label, type })} />
             </li>
           ))}
         </ul>
       )}
-    </div>
+    </Card>
   );
 };

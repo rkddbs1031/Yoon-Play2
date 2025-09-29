a0fa61f4892c1d3e12eabb1a4f01ff128a902930
Kangyoon Lee <rkddbs1031@gmail.com>
2025-09-29 20:32:54 +0900

    Feat: play icon 적용

--- components/Playlist.tsx
+++ components/Playlist.tsx
@@ -1,9 +1,11 @@
 import { ReactNode, useState } from 'react';
 
+import { PlayIcon } from '@/states/icon/svgs';
+
 const Card = ({ children, style }: { children: ReactNode; style?: string }) => {
   return (
     <li
-      className={`relative z-10 flex flex-col gap-4 bg-[#ffffff33] hover:bg-white/70 transition-bg duration-400 px-5 pt-5 pb-6 rounded-[24px] shadow-lg shadow-[0_4px_20px_rgba(0, 0, 0, 0.08)] backdrop-blur-xl w-full cursor-pointer ${style}`}
+      className={`relative z-10 flex flex-col gap-4 bg-[#ffffff33] hover:bg-white/70 transition-bg duration-400 hover:-translate-y-[10px] px-5 pt-5 pb-6 rounded-[24px] shadow-lg shadow-[0_4px_20px_rgba(0, 0, 0, 0.08)] backdrop-blur-xl w-full cursor-pointer ${style}`}
     >
       {children}
     </li>
@@ -14,17 +16,17 @@ interface ContentProps {
   thumbnail: string;
   title: string;
   channelTitle: string;
-  onClick: () => void;
+  onPlay: () => void;
 }
 
-const Content = ({ thumbnail, title, channelTitle, onClick }: ContentProps) => {
+const Content = ({ thumbnail, title, channelTitle, onPlay }: ContentProps) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <button
       type='button'
       className='flex flex-col w-full items-start gap-2 cursor-pointer'
-      onClick={onClick}
+      onClick={onPlay}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -36,7 +38,9 @@ const Content = ({ thumbnail, title, channelTitle, onClick }: ContentProps) => {
             isHovered ? 'opacity-100' : 'opacity-0'
           }`}
         >
-          <button className='px-3 py-1 text-white font-bold bg-red-500 rounded'>Play</button>
+          <span className='px-3 py-1 text-white font-bold '>
+            <PlayIcon color='#ffffff' size={24} />
+          </span>
         </div>
       </div>
       <h3 className='text-[14px] sm:text-4 font-semibold text-left line-clamp-2 md:line-clamp-3'>{title}</h3>
@@ -44,6 +48,7 @@ const Content = ({ thumbnail, title, channelTitle, onClick }: ContentProps) => {
     </button>
   );
 };
+
 export const PlayList = {
   Card: Card,
   Content: Content,

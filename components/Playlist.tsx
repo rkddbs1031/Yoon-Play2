9fc3cae0348756f4232167a11f205601cf5359a9
Kangyoon Lee <rkddbs1031@gmail.com>
2025-10-07 19:28:35 +0900

    Style: Card component 수정

--- components/Playlist.tsx
+++ components/Playlist.tsx
@@ -6,7 +6,7 @@ import { YoutubeThumbnail } from '@/types/youtube';
 const Card = ({ children, style }: { children: ReactNode; style?: string }) => {
   return (
     <li
-      className={`relative z-10 flex flex-col gap-4 bg-white/30 hover:bg-white/70 transition-all duration-400 hover:-translate-y-[10px] p-4 sm:p-5 rounded-2xl shadow-lg shadow-[0_4px_20px_rgba(0, 0, 0, 0.08)] backdrop-blur-xl w-full cursor-pointer ${style}`}
+      className={`relative z-10 flex flex-col gap-4 bg-white/30 hover:bg-white/70 transition-all duration-400 hover:-translate-y-[10px] p-4 sm:p-5 rounded-2xl  shadow-[0px_2px_20px_rgba(0,0,0,0.08)] backdrop-blur-xl w-full cursor-pointer ${style}`}
     >
       {children}
     </li>
@@ -31,8 +31,8 @@ const Content = ({ thumbnail, title, channelTitle, onPlay }: ContentProps) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className='relative w-full'>
-        <img src={thumbnail.high.url} alt={title} className='rounded-[8px] block w-full aspect-square object-cover' />
+      <div className='relative w-full overflow-hidden rounded-[8px]'>
+        <img src={thumbnail.medium.url} alt={title} className='block w-full object-cover' />
 
         <div
           className={`absolute inset-0 flex items-center justify-center bg-black/50 rounded-[8px] transition-opacity duration-400 ${
@@ -44,8 +44,8 @@ const Content = ({ thumbnail, title, channelTitle, onPlay }: ContentProps) => {
           </span>
         </div>
       </div>
-      <h3 className='text-[14px] sm:text-4 font-semibold text-left line-clamp-2'>{title}</h3>
-      <p className='text-[12px] sm:text-[14px] line-clamp-1'>{channelTitle}</p>
+      <h3 className='text-[14px] sm:text-4 font-[500] text-left line-clamp-2'>{title}</h3>
+      <p className='text-[12px] sm:text-[14px] text-[#5f5f7c] line-clamp-1'>{channelTitle}</p>
     </button>
   );
 };

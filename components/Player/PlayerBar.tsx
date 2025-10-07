cf8bbca646ac5aaaf0d8132c8a4142630901998f
Kangyoon Lee <rkddbs1031@gmail.com>
2025-10-07 19:29:54 +0900

    Feat: PlayerBar로 컴포넌트명 변경

--- components/Player/PlayerBar.tsx
+++ components/Player/PlayerBar.tsx
@@ -3,11 +3,11 @@
 import { usePlayer } from '@/hooks/usePlayer';
 import { PlayerControl } from './PlayerControl';
 
-export const MusicPlayer = () => {
-  const { currentVideo } = usePlayer();
+export const PlayerBar = () => {
+  const { currentVideo, togglePlayer } = usePlayer();
 
   return (
-    <section className='fixed z-[100] bottom-5 md:bottom-[32px] left-1/2 -translate-x-1/2 w-[calc(100%-20px)] md:w-[calc(100%-32px)] lg:w-full max-w-[1000px] min-h-[60px]  bg-black/20 backdrop-blur-[20px] rounded-[5px_5px_12px_12px] flex flex-col gap-3 '>
+    <section className='fixed z-[100] w-full bottom-0 left-0 min-h-[60px] bg-white/60 backdrop-blur-[30px] flex flex-col gap-3 cursor-pointer'>
       <PlayerControl.Frame />
       <PlayerControl.ProgressBar />
 
@@ -24,15 +24,15 @@ export const MusicPlayer = () => {
               />
             </div>
             <div className='music-info'>
-              <h4 className='text-[14px] text-white font-bold mb-[2px] line-clamp-1'>{currentVideo.title}</h4>
-              <span className='text-[10px] text-white/70 line-clamp-1'>{currentVideo.channelTitle}</span>
+              <h4 className='text-[14px] text-[#3d3d68] font-[600] mb-[2px] line-clamp-1'>{currentVideo.title}</h4>
+              <span className='text-[10px] text-[#3d3d68] line-clamp-1'>{currentVideo.channelTitle}</span>
             </div>
           </div>
         ) : (
           <div className='w-[50px] h-[50px] rounded-[8px] bg-white/30'></div>
         )}
 
-        <div className='control-wrapper flex items-center gap-2'>
+        <div className='control-wrapper flex items-center justify-between gap-2'>
           <PlayerControl.Buttons />
           <PlayerControl.Volume />
         </div>

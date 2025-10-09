61c969a87cf55ccb0e3325239e0a78fc48935fc0
Kangyoon Lee <rkddbs1031@gmail.com>
2025-10-09 17:01:23 +0900

    Feat: playerbar 배경 이미지 및 트랜지션 적용

--- components/Player/PlayerBar.tsx
+++ components/Player/PlayerBar.tsx
@@ -1,43 +1,81 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import { usePlayer } from '@/hooks/usePlayer';
 import { PlayerControl } from './PlayerControl';
+import { MusicInfoWrapper } from './MusicInfo';
+
+const TRANSITION_DURATION = 400;
 
 export const PlayerBar = () => {
   const { currentVideo, togglePlaylistPanel } = usePlayer();
+  const newImageUrl = currentVideo?.thumbnail?.medium?.url || '';
+
+  const [displayImage, setDisplayImage] = useState<string | null>(null);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    if (!newImageUrl || newImageUrl === displayImage) return;
+
+    const img = new Image();
+    img.src = newImageUrl;
+
+    img.onload = () => {
+      setIsTransitioning(true);
+
+      setTimeout(() => {
+        setDisplayImage(newImageUrl);
+        setIsTransitioning(false);
+      }, TRANSITION_DURATION); // 전환 효과 타이밍 조절
+    };
+
+    img.onerror = () => {
+      setDisplayImage(newImageUrl);
+      setIsTransitioning(false);
+    };
+  }, [newImageUrl]);
 
   return (
     <section
-      className='fixed z-[100] w-full bottom-0 left-0 min-h-[60px] bg-white/60 backdrop-blur-[30px] flex flex-col gap-3 cursor-pointer'
+      className='fixed z-[100] bottom-3 left-1/2 -translate-x-1/2 max-w-[960px] rounded-[8px] w-[calc(100%-32px)] lg:w-full overflow-hidden  min-h-[60px] flex flex-col gap-3 cursor-pointer overflow-hidden'
       onClick={togglePlaylistPanel}
     >
-      <PlayerControl.Frame />
-      <PlayerControl.ProgressBar />
+      {displayImage && (
+        <div
+          className={`absolute inset-0 bg-cover bg-center transform scale-[1.1] filter blur-[10px] brightness-[0.8] 
+            transition-opacity duration-${TRANSITION_DURATION} ease-in-out
+            ${isTransitioning ? 'opacity-50' : 'opacity-100'}
+          `}
+          style={{ backgroundImage: `url(${displayImage})` }}
+        />
+      )}
 
-      <div className='flex items-center gap-3 px-[10px] pb-3'>
-        {currentVideo ? (
-          <div className='music-info-wrapper flex gap-2 items-center'>
-            <div className='thumbnail rounded-[8px] overflow-hidden w-[50px] h-[50px] flex-shrink-0'>
-              <img
-                src={currentVideo.thumbnail.medium.url}
-                alt={currentVideo.title}
-                className='block w-full h-full object-cover'
-                width={50}
-                height={50}
-              />
-            </div>
-            <div className='music-info'>
-              <h4 className='text-[14px] text-[#3d3d68] font-[600] mb-[2px] line-clamp-1'>{currentVideo.title}</h4>
-              <span className='text-[10px] text-[#3d3d68] line-clamp-1'>{currentVideo.channelTitle}</span>
-            </div>
+      <div
+        className={`absolute inset-0 transition-all duration-500 ${displayImage ? 'bg-white/20 backdrop-blur-[10px]' : 'bg-white/60 backdrop-blur-[30px]'} `}
+      />
+
+      <div className='relative z-10 flex flex-col '>
+        <PlayerControl.Frame />
+        {currentVideo && <PlayerControl.ProgressBar />}
+
+        <div className='flex items-center gap-3 px-[10px] py-3 justify-between'>
+          {currentVideo ? (
+            <MusicInfoWrapper
+              thumbnail={currentVideo.thumbnail.medium.url}
+              title={currentVideo.title}
+              channelTitle={currentVideo.channelTitle}
+              titleColor='white'
+              channelTitleColor='white/60'
+            />
+          ) : (
+            <div className='w-[50px] h-[50px] rounded-[8px] bg-black/10'></div>
+          )}
+
+          <div className='control-wrapper flex items-center gap-2'>
+            <PlayerControl.Buttons color='#ffffff' disabledColor={currentVideo ? '#ffffff99' : ''} />
+            <PlayerControl.Volume color='#ffffff' disabledColor={currentVideo ? '#ffffff99' : ''} />
           </div>
-        ) : (
-          <div className='w-[50px] h-[50px] rounded-[8px] bg-black/10'></div>
-        )}
-
-        <div className='control-wrapper flex items-center justify-between gap-2'>
-          <PlayerControl.Buttons />
-          <PlayerControl.Volume />
         </div>
       </div>
     </section>

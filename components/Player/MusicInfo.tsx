5a4dac25a68f46f6a4ef1f734ca77b0c19192634
Kangyoon Lee <rkddbs1031@gmail.com>
2025-10-10 17:37:58 +0900

    Feat: MusicInfo component props 수정

--- components/Player/MusicInfo.tsx
+++ components/Player/MusicInfo.tsx
@@ -1,33 +1,40 @@
+interface ColorAndFontSize {
+  title: string;
+  channelTitle: string;
+}
+
 interface MusicInfoWrapperProps {
   thumbnail: string;
   title: string;
   channelTitle: string;
-  size?: number;
-  titleColor?: string;
-  channelTitleColor?: string;
+  imageSize?: number;
+  color?: ColorAndFontSize;
+  fontSize?: ColorAndFontSize;
 }
 
 const DEFUALT_COLOR = 'text-[#3d3d68]';
+const DEFAULT_TITLE_SIZE = 'text-[14px]';
+const DEFAULT_CHANNEL_TITLE_SIZE = 'text-[10px]';
 
 export const MusicInfoWrapper = ({
   thumbnail,
   title,
   channelTitle,
-  size = 50,
-  titleColor = DEFUALT_COLOR,
-  channelTitleColor = DEFUALT_COLOR,
+  imageSize = 50,
+  color = { title: DEFUALT_COLOR, channelTitle: DEFUALT_COLOR },
+  fontSize = { title: DEFAULT_TITLE_SIZE, channelTitle: DEFAULT_CHANNEL_TITLE_SIZE },
 }: MusicInfoWrapperProps) => {
   return (
     <div className='music-info-wrapper flex gap-2 items-center text-left'>
       <div
         className='thumbnail rounded-[8px] overflow-hidden flex-shrink-0'
-        style={{ width: `${size}px`, height: `${size}px` }}
+        style={{ width: `${imageSize}px`, height: `${imageSize}px` }}
       >
         <img src={thumbnail} alt={title} className='block w-full h-full object-cover' />
       </div>
       <div className='music-info'>
-        <h4 className={`text-[14px] ${titleColor} font-[600] mb-[2px] line-clamp-1`}>{title}</h4>
-        <span className={`text-[10px] ${channelTitleColor} line-clamp-1`}>{channelTitle}</span>
+        <span className={`${fontSize.title} ${color.title} font-[600] mb-[2px] line-clamp-1`}>{title}</span>
+        <span className={`${fontSize.channelTitle} ${color.channelTitle} line-clamp-1`}>{channelTitle}</span>
       </div>
     </div>
   );

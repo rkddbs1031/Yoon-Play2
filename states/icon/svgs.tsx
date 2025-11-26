08b209bccbe825e280eb8d143018fa12eeb4fc23
Kangyoon Lee <rkddbs1031@gmail.com>
2025-11-26 17:36:54 +0900

    Feat: HamburgerIon 추가

--- states/icon/svgs.tsx
+++ states/icon/svgs.tsx
@@ -200,3 +200,14 @@ export const HomeIcon: FC<IconProps> = ({ size = 24, color = 'currentColor' }) =
     </svg>
   );
 };
+
+export const HamburgerIcon: FC<IconProps> = ({ size = 24, color = 'currentColor' }) => {
+  return (
+    <svg width={size} height={size} viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M21 10H3' stroke={color} strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M21 6H3' stroke={color} strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M21 14H3' stroke={color} strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M21 18H3' stroke={color} strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  );
+};

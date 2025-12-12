7d439d9b42b165e47aed588c725f619f569e901c
Kangyoon Lee <rkddbs1031@gmail.com>
2025-12-12 17:42:33 +0900

    Feat: PlaylistView 합성 컴포넌트 추가

---
+++ components/Library/Playlist/PlaylistView/index.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+import { LibraryType } from '@/constants/library';
+
+import ThumbnailGrid from '../../ThumbnailGrid';
+import TrackList from './TrackList';
+
+interface PlaylistViewProps {
+  children: ReactNode;
+}
+
+function PlaylistView({ children }: PlaylistViewProps) {
+  return <section id='playlist-view w-full max-w-[960px]'>{children}</section>;
+}
+
+interface HeaderProps {
+  title: string;
+  thumbnails: string[];
+  count: number;
+  type: LibraryType;
+}
+
+PlaylistView.Header = function Header({ title, thumbnails, count, type }: HeaderProps) {
+  return (
+    <div className='playlist-view-header'>
+      <div className='thumbnail-container max-w-[200px] mx-auto mb-[10px]'>
+        <ThumbnailGrid thumbnails={thumbnails} />
+      </div>
+      <h1 className='text-lg sm:text-xl font-[600] text-[#52527a] text-center whitespace-pre-wrap mb-1'>{title}</h1>
+      <div className='subtitle flex justify-center items-center text-xs sm:text-[12px] text-[#5f5f7c] line-clamp-1'>
+        {type === LibraryType.Playlist && (
+          <span className="relative mr-3 after:absolute after:content-['·'] after:mx-1">재생목록</span>
+        )}
+        <span>{count.toLocaleString()}곡</span>
+      </div>
+    </div>
+  );
+};
+
+PlaylistView.TrackList = TrackList;
+
+export default PlaylistView;

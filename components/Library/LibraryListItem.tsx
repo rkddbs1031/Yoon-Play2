f13ae77497b46c83277a9249a8783103085a5f27
Kangyoon Lee <rkddbs1031@gmail.com>
2025-12-12 17:21:48 +0900

    Feat: LibraryListItem로 컴포넌트명 수정

--- components/Library/LibraryListItem.tsx
+++ components/Library/LibraryListItem.tsx
@@ -2,49 +2,7 @@ import { useCallback } from 'react';
 import Image from 'next/image';
 
 import { LibraryType } from '@/constants/library';
-
-interface ThumbnailGridProps {
-  thumbnails: string[];
-}
-
-function ThumbnailGrid({ thumbnails }: ThumbnailGridProps) {
-  const count = thumbnails.length;
-
-  const getTileImages = useCallback(() => {
-    if (count === 1) return thumbnails;
-    if (count === 2) {
-      const [a, b] = thumbnails;
-      return [a, b, b, a];
-    }
-
-    if (count === 3) {
-      const [a, b, c] = thumbnails;
-      return [a, b, c, a];
-    }
-
-    return thumbnails;
-  }, [thumbnails]);
-
-  const tiles = getTileImages();
-
-  if (count === 1) {
-    return (
-      <div className='thumbnail relative w-full aspect-square rounded-[8px] overflow-hidden '>
-        <Image src={thumbnails[0]} alt='썸네일' fill className='object-cover' />
-      </div>
-    );
-  }
-
-  return (
-    <div className='thumbnail relative w-full aspect-square rounded-[8px] overflow-hidden grid grid-cols-2 grid-rows-2'>
-      {tiles.map((src, idx) => (
-        <div key={`${src}-${idx}`} className='relative w-full h-full'>
-          <Image src={src} alt={`썸네일-${idx}`} fill className='object-cover' />
-        </div>
-      ))}
-    </div>
-  );
-}
+import ThumbnailGrid from './ThumbnailGrid';
 
 interface LibraryListItemProps {
   thumbnails: string[];

ae8fdaa76063ca62451e6ab06a7758dcf7e890ff
Kangyoon Lee <rkddbs1031@gmail.com>
2025-12-18 18:40:45 +0900

    Feat: useLike hook indexedDB 연결

--- hooks/useLike.ts
+++ hooks/useLike.ts
@@ -1,15 +1,34 @@
+import { useEffect } from 'react';
 import { useSetAtom, useAtomValue } from 'jotai';
 
-import { toggleLikePlaylistAtom, isLikedSelectorAtom } from '@/store/likesAtom';
+import { LikeStatus } from '@/constants/like';
+import { toggleLikePlaylistAtom, isLikedSelectorAtom, likedPlaylistAtom } from '@/store/likesAtom';
 import { PlaylistItem } from '@/types/playlist';
 
+import * as likedDB from '@/lib/indexedDB/likedPlaylistDB';
+
 export const useLike = () => {
-  const setLikePlaylist = useSetAtom(toggleLikePlaylistAtom);
+  const toggleLike = useSetAtom(toggleLikePlaylistAtom);
+  const setLikedPlaylist = useSetAtom(likedPlaylistAtom);
   const isLikedSelector = useAtomValue(isLikedSelectorAtom);
+
+  useEffect(() => {
+    likedDB.getLikedPlaylist().then(setLikedPlaylist);
+  }, [setLikedPlaylist]);
+
+  const handleToggleLike = async (item: PlaylistItem) => {
+    const result = toggleLike(item);
+
+    if (result.status === LikeStatus.Add) {
+      await likedDB.addLikedItem(item);
+    } else {
+      await likedDB.deleteLikedItem(item.videoId);
+    }
+
+    return result;
+  };
 
   const isLiked = (videoId: string) => isLikedSelector(videoId);
 
-  const toggleLike = (item: PlaylistItem) => setLikePlaylist(item);
-
-  return { isLiked, toggleLike };
+  return { isLiked, toggleLike: handleToggleLike };
 };

024b817610c9ce708307b976b2da033aca311c2b
Kangyoon Lee <rkddbs1031@gmail.com>
2025-12-21 18:55:25 +0900

    Feat: 플레이리스트용 썸네일 추출 유틸 함수 추가

--- utils/thumbnail.ts
+++ utils/thumbnail.ts
@@ -1,3 +1,4 @@
+import { PlaylistItem } from '@/types/playlist';
 import { ThumbnailSize, YoutubeThumbnail } from '@/types/youtube';
 
 const THUMBNAIL_KEY_MAP: Record<ThumbnailSize, keyof YoutubeThumbnail> = {
@@ -18,4 +19,11 @@ export const formatThumbnailUrl = ({
   const key = THUMBNAIL_KEY_MAP[size];
 
   return thumbnail[key].url;
+};
+
+export const getPlaylistThumbnails = (items: PlaylistItem[], size: ThumbnailSize = 'medium', limit = 4) => {
+  return items
+    .slice(0, limit)
+    .map(item => formatThumbnailUrl({ thumbnail: item.thumbnail, size }))
+    .filter(Boolean);
 };

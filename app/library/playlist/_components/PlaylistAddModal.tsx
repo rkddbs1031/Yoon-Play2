4565afd42b313cb741974f5827020afbab3af681
Kangyoon Lee <rkddbs1031@gmail.com>
2026-02-09 15:44:49 +0900

    feat: PlaylistAddModal 전역 컴포넌트로 수정

--- app/library/playlist/_components/PlaylistAddModal.tsx
+++ app/library/playlist/_components/PlaylistAddModal.tsx
@@ -1,37 +1,36 @@
 'use client';
 
 import { usePlaylist } from '@/hooks/usePlaylist';
-import { usePlaylistCreateModal } from '@/hooks/usePlaylistCreateModal';
+import { usePlaylistAddModal, usePlaylistCreateModal } from '@/hooks/useModal';
 import { PlusIcon, CloseIcon, SpinIcon } from '@/states/icon/svgs';
 import { ModalContent, ModalOverlay, ModalPortal } from '@/components/Modal';
 
-interface PlaylistAddModalProps {
-  onClose: () => void;
-}
-
 // 재생목록 추가 모달 - 1단
-export default function PlaylistAddModal({ onClose }: PlaylistAddModalProps) {
+export default function PlaylistAddModal() {
   const { playlists, isLoading, onAddTrack } = usePlaylist();
-  const { openModal } = usePlaylistCreateModal();
+  const { isOpen, closeModal: onCloseAddModal } = usePlaylistAddModal();
+  const { openModal: onOpenCreateModal } = usePlaylistCreateModal();
 
   const handlePlaylistClick = async (playlistId: string) => {
     await onAddTrack(playlistId);
-    onClose();
+    onCloseAddModal();
   };
+
+  if (!isOpen) return null;
 
   return (
     <ModalPortal>
-      <ModalOverlay onClose={onClose} />
+      <ModalOverlay onClose={onCloseAddModal} />
 
-      <ModalContent className={'relative max-w-[320px] mx-4 '}>
-        <div className='p-4 border-b border-[#66666610] flex items-center justify-between'>
+      <ModalContent className={'relative max-w-[320px] mx-4 pt-[56px]'}>
+        <div className='fixed top-0 w-full flex items-center justify-between p-4 border-b border-[#66666610]'>
           <h2 className='font-[500]'>재생목록 저장</h2>
-          <button type='button' onClick={onClose} className='cursor-pointer border-none bg-none outline-none'>
+          <button type='button' onClick={onCloseAddModal} className='cursor-pointer border-none bg-none outline-none'>
             <CloseIcon size={20} />
           </button>
         </div>
 
-        <div className='border-b py-4 border-[#66666610] pb-[64px]'>
+        <div className='pt-4'>
           <span className='text-xs px-4'>모든 재생목록</span>
           {isLoading ? (
             <div className='flex items-center  justify-center p-4'>
@@ -44,7 +43,7 @@ export default function PlaylistAddModal({ onClose }: PlaylistAddModalProps) {
                   <button
                     type='button'
                     onClick={() => handlePlaylistClick(playlist.id)}
-                    className='w-full flex flex-col items-start gap-1 py-2 px-4 cursor-pointer transition-bg duration-300 hover:bg-white/25                    '
+                    className='w-full flex flex-col items-start gap-1 py-2 px-4 cursor-pointer transition-bg duration-300 hover:bg-white/30                    '
                   >
                     <span className='text-sm text-[#394970] line-clamp-1'>{playlist.title}</span>
                     <span className='text-xs text-[#5f5f7c] line-clamp-1'>{playlist.trackCount}곡</span>
@@ -55,10 +54,10 @@ export default function PlaylistAddModal({ onClose }: PlaylistAddModalProps) {
           )}
         </div>
 
-        <div className='absolute bottom-4 right-4'>
+        <div className='sticky bottom-4 right-0 flex justify-end'>
           <button
-            className='flex gap-1 items-center bg-[currentColor]/90 rounded-full py-1 pl-2 pr-4 cursor-pointer'
-            onClick={openModal}
+            className='flex gap-1 items-center bg-[currentColor]/90 rounded-full py-1 pl-2 pr-4 cursor-pointer mr-4'
+            onClick={onOpenCreateModal}
           >
             <PlusIcon size={20} color='white' />
             <span className='text-xs text-white'>새 재생목록</span>
--- components/ModalRoot.tsx
+++ components/ModalRoot.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import PlaylistAddModal from '@/app/library/playlist/_components/PlaylistAddModal';
 import PlaylistCreateModal from '@/app/library/playlist/_components/PlaylistCreateModal';
 
 export default function ModalRoot() {
-  return <PlaylistCreateModal />;
+  return (
+    <>
+      <PlaylistCreateModal />
+      <PlaylistAddModal />
+    </>
+  );
 }

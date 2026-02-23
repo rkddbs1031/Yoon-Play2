0a9d075e5d80ba84c6da48d878cbd0ff60a3e31d
Kangyoon Lee <rkddbs1031@gmail.com>
2026-02-23 17:53:00 +0900

    refactor: LibraryPlaylist 로직 수정
    좋아요, 유저가 만든 플레이리스트(폴더) 모두 하나의 컴포넌트로 제어되게 수정

--- app/library/playlist/page.tsx
+++ app/library/playlist/page.tsx
@@ -1,13 +1,32 @@
 'use client';
 
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 
-import { LibraryListType } from '@/constants/library';
-import LikedPlaylist from './_components/LikedPlaylist';
+import { fromPlaylistParam } from '@/utils/playlist';
+import PlaylistView from './_components/PlaylistView';
 
 export default function LibraryPlaylist() {
   const searchParams = useSearchParams();
   const listParam = searchParams.get('list');
 
-  if (listParam === LibraryListType.Like) return <LikedPlaylist />;
+  if (!listParam) {
+    return (
+      <section className='w-full h-screen flex flex-col items-center justify-center gap-4 -mt-15'>
+        <h1 className='text-[18px] font-bold'>플레이리스트를 찾을 수 없습니다.</h1>
+        <Link
+          className='w-[140px] py-2 text-center text-[14px] rounded-[32px] backdrop-blur-xl shadow-[0_4px_12px_rgba(0,0,0,0.1)] cursor-pointer transition-bg duration-400 bg-white/20 hover:bg-white/70'
+          href='/'
+        >
+          홈으로 돌아가기
+        </Link>
+      </section>
+    );
+  }
+
+  const playlistId = fromPlaylistParam(listParam);
+
+  if (!playlistId) return null;
+
+  return <PlaylistView playlistId={playlistId} />;
 }

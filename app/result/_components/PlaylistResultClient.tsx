6cc1740cd694942253ff2f278427e6c790f67529
Kangyoon Lee <rkddbs1031@gmail.com>
2026-02-25 18:06:02 +0900

    refactor: 결과 페이지 유튜브 검색 결과 SSR 적용

---
+++ app/result/_components/PlaylistResultClient.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { useEffect, useMemo, useRef } from 'react';
+
+import { usePlayerCore } from '@/hooks/usePlayer';
+import { AnimationType } from '@/constants/animation';
+import { useYoutubeInfiniteQuery } from '@/services/search';
+import { YouTubeSearchList, YoutubeItem } from '@/types/youtube';
+import { PlaylistItem } from '@/types/playlist';
+import { animationStyle } from '@/utils/animation';
+
+import { PlayListCard } from '@/components/Playlist';
+import { Skeleton } from '@/components/Skeleton';
+
+interface PlayListResultClientProps {
+  initialData: YouTubeSearchList;
+  type: string | null;
+  value: string | null;
+}
+
+const OBSERVE_OPTIONS = {
+  root: null,
+  rootMargin: '100px',
+  threshold: 1,
+};
+
+export default function PlaylistResultClient({ initialData, type, value }: PlayListResultClientProps) {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useYoutubeInfiniteQuery({
+    type,
+    value,
+    initialData,
+  });
+
+  const { setPlayerListFromSearch } = usePlayerCore();
+  const target = useRef<HTMLDivElement | null>(null);
+
+  const allItems = useMemo(() => {
+    return data?.pages.flatMap(page => page.items) ?? [];
+  }, [data?.pages]);
+
+  const formatPlaylistItem = (item: YoutubeItem): PlaylistItem => ({
+    videoId: item.id.videoId,
+    title: item.snippet.title,
+    channelTitle: item.snippet.channelTitle,
+    thumbnail: item.snippet.thumbnails,
+  });
+
+  const handlePlay = (clickedItem: YoutubeItem) => {
+    const playlistItems: PlaylistItem[] = allItems.map(formatPlaylistItem);
+    const clicked: PlaylistItem = formatPlaylistItem(clickedItem);
+    setPlayerListFromSearch(playlistItems, clicked);
+  };
+
+  useEffect(() => {
+    if (!target.current) return;
+
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
+        fetchNextPage();
+      }
+    }, OBSERVE_OPTIONS);
+
+    observer.observe(target.current);
+    return () => observer.disconnect();
+  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+  return (
+    <section className='playlist-wrapper max-w-[960px] mx-auto pb-20'>
+      <h1
+        className={`${AnimationType.FadeInUp} text-lg sm:text-xl font-[600] text-[#52527a] mb-6 sm:mb-8 whitespace-pre-wrap`}
+        style={animationStyle({ useAnimation: true, delay: 0.3, duration: 0.6 })}
+      >
+        "{value}" 키워드에 맞는 추천 플레이리스트예요!
+      </h1>
+
+      <div
+        className={`list-wrapper ${AnimationType.FadeInUp}`}
+        style={animationStyle({ useAnimation: true, delay: 0.5, duration: 0.6 })}
+      >
+        <ul className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-6 sm:gap-x-6 sm:gap-y-8 auto-rows-fr'>
+          {allItems.map((item, idx) => (
+            <PlayListCard
+              key={`${item.id.videoId}-${idx}`}
+              idx={idx}
+              thumbnail={item.snippet.thumbnails}
+              title={item.snippet.title}
+              channelTitle={item.snippet.channelTitle}
+              onPlay={() => handlePlay(item)}
+            />
+          ))}
+
+          {isFetchingNextPage && <Skeleton.PlayList length={4} />}
+        </ul>
+
+        <div ref={target} className='h-[50px]'></div>
+      </div>
+    </section>
+  );
+}
--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,109 +1,19 @@
-'use client';
+import { RecommendationType } from '@/constants/recommend';
+import { fetchYoutubeSearch } from '@/services/search';
 
-import { useEffect, useMemo, useRef } from 'react';
-import { useSearchParams } from 'next/navigation';
+import PlaylistResultClient from './_components/PlaylistResultClient';
 
-import { usePlayerCore } from '@/hooks/usePlayer';
-import { AnimationType } from '@/constants/animation';
-import { useYoutubeInfiniteQuery } from '@/services/search';
-import { YoutubeItem } from '@/types/youtube';
-import { PlaylistItem } from '@/types/playlist';
-import { animationStyle } from '@/utils/animation';
-
-import { PlayListCard } from '@/components/Playlist';
-import LoadingSpinner from '@/components/Loading';
-import { Skeleton } from '@/components/Skeleton';
-
-const OBSERVE_OPTIONS = {
-  root: null,
-  rootMargin: '50px',
-  threshold: 1,
-};
-
-export default function PlayListResult() {
-  const searchParams = useSearchParams();
-  const type = searchParams.get('type');
-  const value = searchParams.get('value');
-
-  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } = useYoutubeInfiniteQuery({
-    type,
-    value,
-  });
-
-  const { setPlayerListFromSearch } = usePlayerCore();
-
-  const target = useRef<HTMLDivElement | null>(null);
-
-  const allItems = useMemo(() => {
-    return data?.pages.flatMap(page => page.items) ?? [];
-  }, [data?.pages]);
-
-  const formatPlaylistItem = (item: YoutubeItem): PlaylistItem => ({
-    videoId: item.id.videoId,
-    title: item.snippet.title,
-    channelTitle: item.snippet.channelTitle,
-    thumbnail: item.snippet.thumbnails,
-  });
-
-  const handlePlay = (clickedItem: YoutubeItem) => {
-    const playlistItems: PlaylistItem[] = allItems.map(formatPlaylistItem);
-    const clicked: PlaylistItem = formatPlaylistItem(clickedItem);
-    setPlayerListFromSearch(playlistItems, clicked);
+interface PlayListResultPageProps {
+  searchParams: {
+    type: string;
+    value: string;
   };
+}
 
-  useEffect(() => {
-    if (!target.current) return;
+export default async function PlaylistResultPage({ searchParams }: PlayListResultPageProps) {
+  const { type, value } = searchParams;
 
-    const observer = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
-        fetchNextPage();
-      }
-    }, OBSERVE_OPTIONS);
+  const initialData = await fetchYoutubeSearch({ value, type: type as RecommendationType });
 
-    observer.observe(target.current);
-    return () => observer.disconnect();
-  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
-
-  return (
-    <>
-      <section className='playlist-wrapper max-w-[960px] mx-auto pb-20'>
-        <LoadingSpinner isLoading={isLoading} />
-
-        {!isLoading && data && (
-          <>
-            <h1
-              className={`${AnimationType.FadeInUp} text-lg sm:text-xl font-[600] text-[#52527a] mb-6 sm:mb-8 whitespace-pre-wrap`}
-              style={animationStyle({ useAnimation: true, delay: 0.3, duration: 0.6 })}
-            >
-              "{value}" 키워드에 맞는 추천 플레이리스트예요!
-            </h1>
-            <div
-              className={`list-wrapper ${AnimationType.FadeInUp}`}
-              style={animationStyle({ useAnimation: true, delay: 0.5, duration: 0.6 })}
-            >
-              <ul className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-6 sm:gap-x-6 sm:gap-y-8 auto-rows-fr'>
-                {allItems.map((item, idx) => (
-                  <PlayListCard
-                    key={`${item.id.videoId}-${idx}`}
-                    idx={idx}
-                    thumbnail={item.snippet.thumbnails}
-                    title={item.snippet.title}
-                    channelTitle={item.snippet.channelTitle}
-                    onPlay={() => handlePlay(item)}
-                  />
-                ))}
-                {isFetchingNextPage && (
-                  <>
-                    <Skeleton.PlayList length={2} />
-                  </>
-                )}
-              </ul>
-
-              <div ref={target} className='h-[50px]'></div>
-            </div>
-          </>
-        )}
-      </section>
-    </>
-  );
+  return <PlaylistResultClient initialData={initialData} type={type} value={value} />;
 }

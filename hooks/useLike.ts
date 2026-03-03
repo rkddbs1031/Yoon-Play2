d1916d56e510a2b6e811401f7442db33be8ef488
Kangyoon Lee <rkddbs1031@gmail.com>
2026-03-03 17:43:23 +0900

    refactor: 불필요한 import 및 미사용 코드 제거

--- hooks/useLike.ts
+++ hooks/useLike.ts
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { useSetAtom, useAtomValue, useAtom } from 'jotai';
+import { useEffect } from 'react';
+import { useAtomValue, useAtom } from 'jotai';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { LikeStatus } from '@/constants/like';

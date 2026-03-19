45ec8527736bb6ddea04f6ccfdcadfd791d081e3
Kangyoon Lee <rkddbs1031@gmail.com>
2026-03-19 17:29:44 +0900

    style: PlaylistSkeleton css 수정

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -8,9 +8,9 @@ const PlaylistSkeleton = ({ length }: SkeletonProps) => {
       key={`skeleton-${idx}`}
       className='flex flex-col gap-3 bg-[#ffffff33] p-3 sm:p-4 rounded-2xl shadow-lg shadow-[0_4px_20px_rgba(0, 0, 0, 0.08)] backdrop-blur-xl w-full'
     >
-      <div className='flex-4 animate-pulse rounded-[8px] block w-full  bg-[#ffffff]/40' />
-      <div className='flex-1 animate-pulse rounded-[8px] w-full bg-[#ffffff]/40' />
-      <div className='flex-1 animate-pulse rounded-[8px] w-full  bg-[#ffffff]/40' />
+      <div className='aspect-[16/9] animate-pulse rounded-[8px] block w-full  bg-[#ffffff]/40' />
+      <div className='aspect-[16/3] animate-pulse rounded-[8px] w-full bg-[#ffffff]/40' />
+      <div className='aspect-[16/2] animate-pulse rounded-[8px] w-full  bg-[#ffffff]/40' />
     </li>
   ));
 };

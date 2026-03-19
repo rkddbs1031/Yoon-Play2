87a8deb56fa168686ac566ea2f8c6613698463ff
Kangyoon Lee <rkddbs1031@gmail.com>
2026-03-19 18:19:45 +0900

    style: ReusltLoading 레이아웃 수정

--- app/result/loading.tsx
+++ app/result/loading.tsx
@@ -3,6 +3,8 @@ import { Skeleton } from '@/components/Skeleton';
 export default function ReusltLoading() {
   return (
     <div className='result-laoding'>
+      <div className='animate-pulse rounded-[8px] bg-[#ffffff55] mb-6 sm:mb-8 h-8 max-w-[250px]' />
+
       <ul className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-6 sm:gap-x-6 sm:gap-y-8 auto-rows-fr'>
         <Skeleton.PlayList length={4} />
       </ul>
